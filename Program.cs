0c9c4f729d16490c02bb2fc577e15a222cd580cf
Dezyuk <romanyuk.groz@gmail.com>
2023-11-30 10:55:10 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using lab11;
+using System.Diagnostics;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Person[] persons = new Person[10000];
+
+        TaskManager taskManager = new TaskManager();
+        var taskInfo = taskManager.Generate(persons, 8);
+
+        await taskInfo;
+
+        FilterPersons filter = new FilterPersons();
+
+        var result = filter.GetGenderGroupsByName(persons, "a", 15);
+
+
+    }
+}
---
+++ TaskManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace lab11;
+
+public class TaskManager
+{
+    public async Task Generate(Person[] persons, int threadNumber)
+    {
+        if (persons != null && threadNumber > 0)
+        {
+            int chunkSize = persons.Length / threadNumber;
+            Task[] tasks = new Task[threadNumber];
+            for (int i = 0; i < threadNumber; i++)
+            {
+                var startIndex = i * chunkSize;
+                var endIndex = (i == 3) ? persons.Length : (i + 1) * chunkSize;
+
+                tasks[i] = GeneratePersons(persons, startIndex, endIndex);
+            }
+            await Task.WhenAll(tasks);
+        }
+        else
+        {
+            throw new ArgumentNullException("Element can not be null");
+        }
+    }
+
+    private Task GeneratePersons(Person[] persons, int startIndex, int endIndex)
+    {
+        for (var i = startIndex; i < endIndex; i++)
+        {
+            persons[i] = new Person()
+            {
+                Id = i,
+                Name = GenerateName(),
+                Age = Random.Shared.Next(1, 100),
+                Gender = Random.Shared.Next(1, 3)
+            };
+        }
+        
+        return Task.CompletedTask;
+    }
+
+    private string GenerateName()
+    {
+        int length = Random.Shared.Next(5, 8);
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++)
+        {
+            builder.Append((char)Random.Shared.Next(97, 123));
+        }
+
+        builder[0] = char.ToUpper(builder[0]);
+        return builder.ToString();
+    }
+}
